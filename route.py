2c1b10ba150db90ccb7b6a8aa18af6b23969fee7
Shruti Rachh <shrutirachh@shrutis-mbp.attlocal.net>
2017-12-02 13:30:49 -0600

    Added astar routing algorithm

--- route.py
+++ route.py
@@ -17,6 +17,114 @@ maxSpeed=0
 maxDist=0
 
 '''
+Computes all the routing options (distance,time,segment,scenic) and path for a city 
+based on the computations of its parent city and returns a dictionary.
+Input:
+	currCity- name of the city that is currently being explored.
+	adjCity- one of the neighboring city of currCity
+	d- list of [dist,speed_limit,highway] of adjCity
+	dist- dictionary containing all the computations (distance,time,segment,scenic,path) 
+		  of the visited cities
+Output:
+	a dictionary containing the computation for adjCity 
+'''
+def compute(currCity,adjCity,d,dist):
+	currPath=dist[currCity]["path"]+"\n"+adjCity
+	currTime=0
+	distOnHighway=dist[currCity]["scenic"]
+	if d[0]!="" and d[1]!="" and d[0]!="0" and d[1]!="0":
+		currTime=round((float(d[0])/float(d[1]))*60,2)
+		if int(d[1])>=55:
+			distOnHighway+=int(d[0])
+	else:
+		return {}
+	return {"distance":int(d[0])+dist[currCity]["distance"],"time":currTime+dist[currCity]["time"],"segment":dist[currCity]["segment"]+1,"scenic":distOnHighway,"path":currPath}	
+
+'''
+Reads the information about the latitudes and longitudes of the cities
+from city-gps.txt file and stores it in the dictionary.
+Output:
+	a dictionary with key as the city and value as the list of [latitude,longitude] 
+'''
+def getGeoInfo():
+	geoInfo={}
+	with open('city-gps.txt','rb') as file:
+		for cityInfo in file:
+			record=cityInfo.split(" ")
+			geoInfo[record[0]]=[record[1],record[2]]
+	return geoInfo
+
+'''
+Reads the information about the latitudes and longitudes of the cities
+from city-gps.txt file and stores it in the dictionary.
+Output:
+	a dictionary with key as the city and value as the list of [latitude,longitude] 
+'''
+def getBestNode(visitedCities,endCity,dist,routingOpt):
+	minCost=sys.maxint
+	bestCity=""
+	for city in visitedCities:
+		if minCost>dist[city]["heuristic"]:
+			minCost=dist[city]["heuristic"]
+			bestCity=city
+	return bestCity
+
+'''
+This function is called when there are no geographical co-ordinates for a city.
+It finds whether the neighbors of the city has geographical co-ordinates, if yes
+it returns that neighbor.
+Input:
+	city- name of the city whose latitude and longitude is not known 
+	geoInfo- dictionary containing information about the latitudes and longitudes of the cities 
+	mapData- dictionary containing the list of cities along with the information about their 
+			 neighboring cities
+Output:
+	a dictionary with key as the neigbor city and value as the list of [distance,speed_limit,highway]
+	retrieved from mapData dictionary whose geographical co-ordinates are
+	present in the geoInfo dictionary,if no such neighbor found, it returns empty dict 
+'''
+def findNeighbor(city,geoInfo,mapData):
+	for neighbor in mapData[city]:
+		if neighbor.keys()[0] in geoInfo.keys():
+			return neighbor
+	return {}
+
+'''
+Calculates the heuristic function depending on whether the geographical co-ordinates are present
+or not. If not present, it checks if its parent or any of the neighboring cities contains the
+geographical co-ordinates. If none present, then it assigns max int value to heuristic function.
+Input:
+	currCity- parent city of the city whose heuristic function is currently being computed
+	adjCity- city whose heuristic is to be computed
+	geoInfo- dictionary containing information about the latitudes and longitudes of the cities 
+	mapData- dictionary containing the list of cities along with the information about their 
+			 neighboring cities
+	dist- dictionary containing all the computations (distance,time,segment,scenic,path) 
+		  of the visited cities
+Output:
+	heuristic function value
+''' 
+def getHeuristic(currCity,adjCity,geoInfo,mapData,dist):
+	if adjCity in geoInfo.keys():
+		h=computeHeuristic(geoInfo[adjCity],geoInfo[endCity])
+	elif currCity in geoInfo.keys():
+		h=computeHeuristic(geoInfo[currCity],geoInfo[endCity])-(dist[adjCity][routingOpt]-dist[currCity][routingOpt])
+	else: 
+		n=findNeighbor(adjCity,geoInfo,mapData)
+		if n:
+			nData=compute(adjCity,n.keys()[0],n.values()[0],dist)
+			h=computeHeuristic(geoInfo[n.keys()[0]],geoInfo[endCity])+(nData[routingOpt]-dist[adjCity][routingOpt])		
+		else:
+			h=sys.maxint
+	if routingOpt=="time":
+		h=h/float(maxSpeed)
+	elif routingOpt=="segment":
+		h=int(h/float(maxDist))
+	elif routingOpt=="scenic":
+		h=h/2
+	return h
+
+'''
 Computes the path to the end city by exploring the neighbors of 
 each of the cities starting with the start city and computing the cost 
 (distance,time,scenic,segment) for each of them
@@ -34,7 +142,7 @@ Output:
 	dictionary containing the computed (distance,time,scenic,segment,path) for each of the visited
 	cities and list of visited cities.
 ''' 
-def getPath(currCity,dist,visitedCities,mapData):
+def getPath(currCity,dist,visitedCities,mapData,geoInfo={}):
 	for neighbor in mapData[currCity]:
 		city=neighbor.keys()[0]
 		d=neighbor.values()[0]
@@ -48,6 +156,9 @@ def getPath(currCity,dist,visitedCities,mapData):
 				visitedCities.append(city)
 			elif int(d[0])+dist[currCity][routingOpt]<dist[city][routingOpt]:
 				dist[city]=currData
+			if geoInfo and city in dist.keys():
+				h=getHeuristic(currCity,city,geoInfo,mapData,dist)
+				dist[city]["heuristic"]=h+dist[city][routingOpt]
 	return visitedCities,dist
 
 '''
@@ -117,6 +228,30 @@ def ids(mapData):
 	return path
 
 '''
+Runs A* algorithm to find a path between start city and end city, if any based on some heuristic
+function. It may or may not return an optimal solution depending on the given routing option.
+Input:
+	mapData- dictionary containing the list of cities along with the information about their 
+			 neighboring cities
+Output:
+	dictionary containing the computed (distance,time,scenic,segment,path) for the end-city
+'''
+def astar(mapData):
+	visitedCities=[startCity]
+	dist={}
+	dist[startCity]={"distance":0,"time":0,"segment":0,"scenic":0,"path":startCity,"heuristic":0}
+	geoInfo=getGeoInfo()
+	while len(visitedCities)>0:
+		currCity=getBestNode(visitedCities,endCity,dist,routingOpt)
+		visitedCities.remove(currCity)
+		getPath(currCity,dist,visitedCities,mapData,geoInfo)
+		if endCity in dist.keys():
+			break
+	if endCity not in dist.keys():
+		return {}
+	return dist[endCity]
+
+'''
 Builds a graph storing the distances of adjacent cities in a dictionary	
 considering that all roads are bi-directional by reading from 
 road-segments.txt file.
@@ -129,6 +264,13 @@ def buildMap():
 	with open('road-segments.txt','rb') as file:
 		for routes in file:
 			record=routes.split(" ")
+
+			# max speed and max distance is computed to calculate time and segment heuristic in case of astar
+			global maxSpeed,maxDist
+			if maxSpeed<record[3]:
+				maxSpeed=record[3]
+			if maxDist<record[2]:
+				maxDist=record[2]
 			if record[0] not in mapData.keys():
 				mapData[record[0]]=[{record[1]:[record[2],record[3],record[4]]}]
 			else:
@@ -154,6 +296,11 @@ def findRoute(mapData):
 		return bfs(mapData)
 	elif routingAlgo=="dfs":
 		return dfs(mapData)
+	elif routingAlgo=="ids":
+		return ids(mapData)
+	elif routingAlgo=="astar":
+		return astar(mapData)
+
 
 # Runs the code
 if routingOpt not in ["distance","time","scenic","segment"]:
